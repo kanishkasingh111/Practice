81036f801bc0539c4e05e0a421691467086e8e37
Kanishka Singh <singhkanishka228@gmail.com>
2025-10-29 20:03:35 +0530

    Add files via upload

---
+++ character_triangle.cpp
@@ -0,0 +1,22 @@
+/*Character Triangle
+A
+AB
+ABC
+ABCD
+ABCDE
+*/ 
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"Enter n : ";
+    cin>>n;
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=i;j++){
+            cout<<char(j+64);
+        }
+        cout<<"\n";
+    }
+}
