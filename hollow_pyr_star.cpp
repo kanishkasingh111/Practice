34e49b9321cd2400b166225d859d57f0af4157ee
Kanishka Singh <singhkanishka228@gmail.com>
2025-10-31 22:26:11 +0530

    Add files via upload

---
+++ hollow_pyr_star.cpp
@@ -0,0 +1,28 @@
+/* Hollow Pyramid Star Pattern 
+    *
+   * *
+  *   *
+ *     *
+*********
+*/
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"Enter n : ";
+    cin>>n;
+    for(int i=1;i<=n;i++){
+          for (int j = i; j < n; j++) {
+            cout << " ";
+        }
+        for(int j=1;j<=(2*i-1);j++){
+            if(j==1 || j==(2*i-1) || i==n)
+            cout<<"*";
+            else
+            cout<<" ";
+        }
+        cout<<"\n";
+    }
+}
