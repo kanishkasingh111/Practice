1f99985da7085e47e53186f0da41896225581a64
Kanishka Singh <singhkanishka228@gmail.com>
2025-11-01 22:12:17 +0530

    hollow_pyr_star.cpp

--- hollow_pyr_star.cpp
+++ hollow_pyr_star.cpp
@@ -17,12 +17,13 @@ int main()
           for (int j = i; j < n; j++) {
             cout << " ";
         }
-        for(int j=1;j<=(2*i-1);j++){
-            if(j==1 || j==(2*i-1) || i==n)
+        for(int j = 1;j <= (2*i-1);j++){
+            if(j == 1 || j == (2*i-1) || i == n)
             cout<<"*";
             else
             cout<<" ";
         }
         cout<<"\n";
     }
+
 }
